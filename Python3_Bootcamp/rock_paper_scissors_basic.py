1a0359fd8e69f858549ca344bc32727195940b49
Joseph Clay <clayjoseph1994@gmail.com>
2021-01-06 16:32:51 -0500

    refactored code for simplicity and added some extra components. This is version 2 of the simple RPS program I created.

--- Python3_Bootcamp/rock_paper_scissors_basic.py
+++ Python3_Bootcamp/rock_paper_scissors_basic.py
@@ -9,26 +9,36 @@ print("Please enter name for Player 2: ")
 p2_name = input()
 
 p1 = input("Player 1, make your move. Choose 'rock', 'paper', or 'scissors': \n")
+print("**********NO CHEATING***********\n\n" * 20)
 p2 = input("Player 2, make your move. Choose 'rock', 'paper', or 'scissors': \n")
 
-#Player 1 options
-#p1 as rock
+#Tie game
 if p1 == p2:
 	print("Tie Game. Try again!")
-elif p1 == "rock" and p2 == "paper":
-	print("Paper covers rock; " + p2_name + " wins!")
-elif p1 == "rock" and p2 == "scissors":
-	print("Rock crushes scissors; " + p1_name + " wins!")
-#p1 as paper	
-elif p1 == "paper" and p2 == "rock":
-	print("Paper covers rock; " + p1_name + " wins!")
-elif p1 == "paper" and p2 == "scissors":
-	print("Scissors cuts paper; " + p2_name + " wins!")
+#p1 as rock
+elif p1 == "rock":
+	if p2 == "paper":
+		print("Paper covers rock; " + p2_name + " wins!")
+	elif p2 == "scissors":
+		print("Rock crushes scissors; " + p1_name + " wins!")
+	else:
+		print(p2_name + "- Something went wrong; Maybe check spelling or don't capitalize your choices?")
+#p1 as paper
+elif p1 == "paper":
+	if p2 == "rock":
+		print("Paper covers rock; " + p1_name + " wins!")
+	elif p2 == "scissors":
+		print("Scissors cuts paper; " + p2_name + " wins!")
+	else:
+		print(p2_name + "- Something went wrong; Maybe check spelling or don't capitalize your choices?")
 #p1 as scissors
-elif p1 == "scissors" and p2 == "rock":
-	print("Rock crushes scissors; " + p2_name + " wins!")
-elif p1 == "scissors" and p2 == "paper":
-	print("Scissors cuts paper; " + p1_name + " wins!")
-#counting for typos or other wildcards
+elif p1 == "scissors":
+	if p2 == "rock":
+		print("Rock crushes scissors; " + p2_name + " wins!")
+	elif p2 == "paper":
+		print("Scissors cuts paper; " + p1_name + " wins!")
+	else:
+		print(p2_name + "- Something went wrong; Maybe check spelling or don't capitalize your choices?")
+#Acccounting for typos or other wildcards
 else:
-	print("Something went wrong; Maybe check spelling or don't capitalize your choices?")
+	print("- Something went wrong; Maybe check spelling or don't capitalize your choices?")
