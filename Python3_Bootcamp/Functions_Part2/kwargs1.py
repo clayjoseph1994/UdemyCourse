c8524e422ff192266119570b5ec6cad9cd1d4898
Joseph Clay <clayjoseph1994@gmail.com>
2021-04-26 10:56:32 -0400

    modifying module to remove duplicate printing

--- Python3_Bootcamp/Functions_Part2/kwargs1.py
+++ Python3_Bootcamp/Functions_Part2/kwargs1.py
@@ -2,4 +2,4 @@ def fav_colors(**kwargs):
     for person, color in kwargs.items():
         print(f"{person}'s favorite color is {color}")
 
-print(fav_colors(joseph="purple", ruby="red", ethel="teal"))
+fav_colors(joseph="purple", ruby="red", ethel="teal")
