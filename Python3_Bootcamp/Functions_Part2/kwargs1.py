a6152dde94e46f937bb9e49801a827b3c1e22c6c
Joseph Clay <clayjoseph1994@gmail.com>
2021-04-26 11:21:04 -0400

    adding new function to kwargs

--- Python3_Bootcamp/Functions_Part2/kwargs1.py
+++ Python3_Bootcamp/Functions_Part2/kwargs1.py
@@ -2,4 +2,18 @@ def fav_colors(**kwargs):
     for person, color in kwargs.items():
         print(f"{person}'s favorite color is {color}")
 
-fav_colors(joseph="purple", ruby="red", ethel="teal")
+fav_colors(Joseph="purple", Justyce="pink", Nancy="teal")
+
+print("=================================================")
+
+def special_greeting(**kwargs):
+    if "Joseph" in kwargs and kwargs["Joseph"] == "special":
+        return "You get a special greeting, Joseph!"
+    elif "Joseph" in kwargs:
+        return f'{kwargs["Joseph"]} Joseph!'
+
+    return "Not sure who this is..."
+
+print(special_greeting(Joseph="Hello", Justyce="special"))
+print(special_greeting(Bob="Hello"))
+print(special_greeting(Joseph="special", Justyce="Hello"))
